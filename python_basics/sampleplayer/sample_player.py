e86f9f0748bfec2272d43c3ae7f818435cac7342
Ruben Lijbers <rbngemail@gmail.com>
2018-09-18 23:40:36 +0200

    added two simple sample play scripts

--- python_basics/sampleplayer/sample_player.py
+++ python_basics/sampleplayer/sample_player.py
@@ -1,4 +1,15 @@
+import time as tm
 import simpleaudio as sa
+# numPlayBackTimes
+# BPM
+def play(sample):
+    wave_obj = sa.WaveObject.from_wave_file(sample)
+    play_obj = wave_obj.play()
+    play_obj.wait_done()
 
-numPlaybackTimes
-BPM
+sampleSel = input("Which sample?  ")
+#playAmt = input("How many times?  ")
+#playInt = input("At which time BPM?  ")
+
+play(sampleSel)
+
---
+++ python_basics/sampleplayer/sample_repeater.py
@@ -0,0 +1,17 @@
+import simpleaudio as sa
+import time as tm
+sampleSel = input("Which sample? ")
+repAmt = int(input("How many times? "))
+repInt = float(input("At what interval? "))
+
+def repeat(play, sampleSel, repAmt, repInt):
+    for i in range(repAmt):
+        play(sampleSel)
+        tm.sleep(repInt)
+
+def play(sample):
+    wave_obj = sa.WaveObject.from_wave_file(sample)
+    play_obj = wave_obj.play()
+    play_obj.wait_done()
+
+repeat(play, sampleSel, repAmt, repInt)
